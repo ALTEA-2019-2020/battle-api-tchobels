73e9e5efd562f058eaf6ccf8f629b42a6f6d2fc0
Alexis <alexis.bels@outlook.fr>
2020-04-04 16:44:21 +0200

    FIX: secu

--- src/main/java/com/miage/altea/tp/battle_api/config/SecurityConfig.java
+++ src/main/java/com/miage/altea/tp/battle_api/config/SecurityConfig.java
@@ -11,48 +11,43 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Optional;
 
-    @Configuration
-    public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-        @Autowired
-        private TrainerService trainerService;
+    @Autowired
+    private TrainerService trainerService;
 
-        public TrainerService getTrainerService() {
-            return trainerService;
-        }
-
-        @Autowired
-        public void setTrainerService(TrainerService trainerService) {
-            this.trainerService = trainerService;
-        }
-
-        @Bean
-        public PasswordEncoder passwordEncoder() {
-            return new BCryptPasswordEncoder();
-        }
-
-        @Bean
-        public UserDetailsService userDetailsService() {
-            return userName -> Optional.ofNullable(trainerService.getTrainerByName(userName))
-                    .map((trainer) ->
-                            User.withUsername(trainer.getName())
-                                    .password(trainer.getPassword())
-                                    .roles("USER")
-                                    .build()
-                    ).orElseThrow(() -> new BadCredentialsException("No such user"));
-        }
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            super.configure(http);
-            http.csrf().disable();
-        }
+    public TrainerService getTrainerService() {
+        return trainerService;
     }
+
+    @Autowired
+    public void setTrainerService(TrainerService trainerService) {
+        this.trainerService = trainerService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return userName -> Optional.ofNullable(trainerService.getTrainerByName(userName))
+                .map((trainer) ->
+                        User.withUsername(trainer.getName())
+                                .password(trainer.getPassword())
+                                .roles("USER")
+                                .build()
+                ).orElseThrow(() -> new BadCredentialsException("No such user"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.csrf().disable();
+    }
+}
